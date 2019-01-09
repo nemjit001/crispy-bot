b1c23690487e5b9152ab19589ce57e13445e4c2f
Tijmen Verhoef <Tijmenmenno@gmail.com>
2019-01-09 20:46:08 +0100

    added some function to the cpp file

--- cppFiles/controlScript.cpp
+++ cppFiles/controlScript.cpp
@@ -2,6 +2,7 @@
 #include <cstdlib>
 
 /*
+ *
  * TODO:
  * based on velocity calc time needed before turning?
  * write camera function
@@ -9,7 +10,17 @@
  *
  */
 
+// Declaring all functions
+void steer(int deg);
+int calcDeg();
+int timeToTurn(int velocity, int distance);
+
+// Declaring global variables
+AnalogIn accelerometer(p20);
+serial terminal(USBTX, USBRX);
+
 int main() {
+	int speed = 0;
 	try {
 		while (true) {
 			/* write drive chain */
@@ -19,4 +30,25 @@ int main() {
 		std::cout << "Runtime Error: " << excpt.what() << std::endl;
 	}
     return 0;
+}
+
+void steer(int deg, int speed){
+	if (deg >= -360 && deg <= 360) {
+		wait_ms(timeToTurn());
+	}
+	else {
+		throw std::runtime_error("servo value out of range!\r\nStopping Program");
+	}
+}
+
+int calcDeg () {
+	int degrees = 0;
+
+	// take in cam data and calculate degrees
+
+	return degrees;
+}
+
+int timeToTurn (int velocity, int distance) {
+	return (distance / velocity) * 1000;
 }
