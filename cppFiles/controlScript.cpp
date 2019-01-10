3ca50f0a1054c36be55b72374d9eab8108381112
Tijmen Verhoef <Tijmenmenno@gmail.com>
2019-01-10 11:18:34 +0100

    added comments to controlScript.cpp

--- cppFiles/controlScript.cpp
+++ cppFiles/controlScript.cpp
@@ -20,23 +20,35 @@ serial terminal(USBTX, USBRX);
 int carSpeed = 0;
 
 int main() {
+	bool drive = true;
 	try {
-		while (true) {
+		while (drive) {
 			/* write drive chain */
+			if (/* no turn */) {
+				// set motor values to drive with speed carSpeed;
+			}
+			else if (/* turn */) {
+				steer(calcDeg(), carSpeed);
+			}
+			else {
+				drive = false;
+				carSpeed = 0;
+			}
 		}
+		// stop all motors and shut down cam
 	}
 	catch (std::runtime_error& excpt) {
-		std::cout << "Runtime Error: " << excpt.what() << std::endl;
+		terminal.printf("Runtime Error: %s", excpt.what());
 	}
     return 0;
 }
 
 void steer(int deg, int speed){
 	if (deg >= -360 && deg <= 360) {
-		wait_ms(timeToTurn());
+		// Set servo to degree value
 	}
 	else {
-		throw std::runtime_error("servo value out of range!\r\nStopping Program");
+		throw std::runtime_error("servo value out of range!\r\nStopping Program...");
 	}
 }
 
