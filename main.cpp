6dff140f47da68ef3ab23ec6e2b25322e3f9db6f
Stijn Winter <stijnjw11@gmail.com>
2019-04-23 11:09:28 +0200

    stinnaw nieuwe main

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@
 #include "MW_DC_Motor.h"
 
 Serial terminal(USBTX, USBRX); // tx, rx
-int threshold;
-float speed = 0.125;
+int threshold, min = 1210, max = 1210;
+float diff = 2000;
 PwmOut servo = PwmOut(PTA12);
 MW_DC_Motor motorA = MW_DC_Motor('A');
 MW_DC_Motor motorB = MW_DC_Motor('B');
@@ -18,19 +18,18 @@ int getMid(uint16_t* cameraDataVector, int prev_mid){
 
     mean = mean / 40;
     threshold = mean * 0.71;
-    //terminal.printf("Threshold: %f\n", threshold);
 
     //Setting first edge
     for(int i = 64; i > 0; --i){
-        if(cameraDataVector[i] < threshold){
+        if(abs(cameraDataVector[i] - cameraDataVector[i - 1]) > diff){
             firstEdge = i;
             break;
         }
     }
 
     //Setting second edge
-    for(int i = 64; i < 128; ++i){
-        if(cameraDataVector[i] < threshold){
+    for(int i = 64; i < 127; ++i){
+        if(abs(cameraDataVector[i] - cameraDataVector[i + 1]) > diff){
             secEdge = i;
             break;
         }
@@ -40,18 +39,24 @@ int getMid(uint16_t* cameraDataVector, int prev_mid){
 }
 
 int setWheels(int mid){
-    int neutral = 1210, dist = 150, pos;
-    float offset, magic = 60, min = 0.02;
+    int neutral = 1210, dist = 160, pos;
+    float offset, magic = 30, speed = 0.125;
     
     offset = -float(mid - 64) / 64.0;
-    if (abs(offset) > min) offset = 0;
     
-    pos = neutral + offset * float(dist) * magic;
+    offset = (offset >= 0) ? pow(offset, 2.5f) : -pow(-offset, 2.5f);
+    
+    pos = neutral + offset * dist * magic;
 
     if(pos > neutral + dist) pos = neutral + dist;
     else if(pos < neutral - dist) pos = neutral - dist;
     
     servo.pulsewidth_us(pos);
+    
+    //speed -= abs(offset) * 0.6;
+    motorA.setSpeed(speed);
+    motorB.setSpeed(-speed);
+
     return mid;
 }
 
@@ -70,26 +75,16 @@ int main(){
     int prev_mid = 64, mid = 64;
     MW_Camera camera = MW_Camera(0);
     servo.period(0.02f);
+    bool stopped = false;
     
-    camera.setExposureTime(10);
+    camera.setExposureTime(2);
     
-    motorA.setSpeed(speed);
-    motorB.setSpeed(-speed);
-    
-    while(true){
-        if (prev_mid > 62 && prev_mid < 66) {
-            motorA.setSpeed(-speed);
-            motorB.setSpeed(speed);
-        }
-        else {
-            motorA.setSpeed(speed);
-            motorB.setSpeed(-speed);
-        }
+    while(!stopped){
         camera.updateCameraImage();
         camera.getCameraImage(cameraDataVector);
         mid = getMid(cameraDataVector, prev_mid);
         prev_mid = setWheels(mid);
-        drawCam(cameraDataVector);
+        //drawCam(cameraDataVector);
     }
 
     return 0;   
