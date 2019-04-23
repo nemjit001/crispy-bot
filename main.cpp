81a7d397488d51e4213bb87ebb017f2ab86839b0
Tijmen Verhoef <tijmenmenno@gmail.com>
2019-04-23 11:43:39 +0200

    Made edits to main.cpp
    Changes made:
    - setWheels() is now a void, as prev_mid was an unused var
    - mean is now a float to avoid truncation
    - other unused vars were removed

--- main.cpp
+++ main.cpp
@@ -2,26 +2,30 @@
 #include "MW_Camera.h"
 #include "MW_DC_Motor.h"
 
-Serial terminal(USBTX, USBRX); // tx, rx
-int threshold, min = 1210, max = 1210;
+#define SPEED 0.125
+
+Serial terminal(USBTX, USBRX); // gebruikt usb connectie voor screen
+int threshold;
 float diff = 2000;
 PwmOut servo = PwmOut(PTA12);
 MW_DC_Motor motorA = MW_DC_Motor('A');
 MW_DC_Motor motorB = MW_DC_Motor('B');
 
-int getMid(uint16_t* cameraDataVector, int prev_mid){
-    int firstEdge = 0, secEdge = 127, mean = 0;
+float getMid(uint16_t* cameraDataVector){
+    int firstEdge = 0, secEdge = 127;
+    float mean = 0;
 
     for(int i = 40; i < 80; ++i){
         mean += cameraDataVector[i];
     }
 
-    mean = mean / 40;
-    threshold = mean * 0.71;
+    mean /= 40.0;
+    threshold = mean * 0.071;
+    terminal.printf("%f\r\n", mean);
 
     //Setting first edge
     for(int i = 64; i > 0; --i){
-        if(abs(cameraDataVector[i] - cameraDataVector[i - 1]) > diff){
+        if(abs(cameraDataVector[i] - cameraDataVector[i - 1]) > threshold){
             firstEdge = i;
             break;
         }
@@ -29,18 +33,18 @@ int getMid(uint16_t* cameraDataVector, int prev_mid){
 
     //Setting second edge
     for(int i = 64; i < 127; ++i){
-        if(abs(cameraDataVector[i] - cameraDataVector[i + 1]) > diff){
+        if(abs(cameraDataVector[i] - cameraDataVector[i + 1]) > threshold){
             secEdge = i;
             break;
         }
     }
     
-    return ((firstEdge + secEdge) / 2);
+    return (firstEdge == 0 && secEdge == 127) ? 64.0 : ((firstEdge + secEdge) / 2.0);
 }
 
-int setWheels(int mid){
+void setWheels(float mid){
     int neutral = 1210, dist = 160, pos;
-    float offset, magic = 30, speed = 0.125;
+    float offset, magic = 30;
     
     offset = -float(mid - 64) / 64.0;
     
@@ -48,31 +52,29 @@ int setWheels(int mid){
     
     pos = neutral + offset * dist * magic;
 
-    if(pos > neutral + dist) pos = neutral + dist;
-    else if(pos < neutral - dist) pos = neutral - dist;
+    if (pos > neutral + dist) pos = neutral + dist;
+    else if (pos < neutral - dist) pos = neutral - dist;
     
     servo.pulsewidth_us(pos);
     
     //speed -= abs(offset) * 0.6;
-    motorA.setSpeed(speed);
-    motorB.setSpeed(-speed);
-
-    return mid;
+    motorA.setSpeed(SPEED);
+    motorB.setSpeed(-SPEED);
 }
 
 
 void drawCam(uint16_t* cameraDataVector) {
     for (int i = 0; i < 128; i++) {
-        if (cameraDataVector[i] < threshold) printf("#");
-        else printf("-");
+        if (cameraDataVector[i] < threshold) terminal.printf("#");
+        else terminal.printf("-");
     }
-    printf("\r\n");
+    terminal.printf("\r\n");
 }
 
 
 int main(){
     uint16_t cameraDataVector[128];
-    int prev_mid = 64, mid = 64;
+    float mid = 64.0;
     MW_Camera camera = MW_Camera(0);
     servo.period(0.02f);
     bool stopped = false;
@@ -82,8 +84,9 @@ int main(){
     while(!stopped){
         camera.updateCameraImage();
         camera.getCameraImage(cameraDataVector);
-        mid = getMid(cameraDataVector, prev_mid);
-        prev_mid = setWheels(mid);
+        mid = getMid(cameraDataVector);
+        setWheels(mid);
+        
         //drawCam(cameraDataVector);
     }
 
