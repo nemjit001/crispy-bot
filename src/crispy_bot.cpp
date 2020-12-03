786156e4c947d23f4e2c33f8f250db0157c05ac9
Tijmen Verhoef <tijmenmenno@gmail.com>
2020-12-03 15:25:47 +0100

    working on Bot class

--- src/crispy_bot.cpp
+++ src/crispy_bot.cpp
@@ -1,8 +1,34 @@
+#include "gpio.h"
 #include "Motor.h"
 #include "util.h"
 
+class Bot
+{
+    private:
+    Motor *motor_a;
+    Motor *motor_b;
+
+    public:
+    Bot();
+    ~Bot();
+};
+
+Bot::Bot()
+{
+    this->motor_a = new Motor(PTA01, PTA02);
+    this->motor_b = new Motor(PTA03, PTA04);
+}
+
+Bot::~Bot()
+{
+    delete this->motor_a;
+    delete this->motor_b;
+}
+
 int main()
 {
+    Bot *bot = new Bot();
 
+    delete bot;
     return 0;
 }
